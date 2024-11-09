65c2719f47ddcc4523adcd66e7bc141258bf2bb1
Giovanni Bernardo <canaltamojuntooooo@gmail.com>
2024-11-09 11:28:49 -0300

    first commit

---
+++ src/test/java/com/br/sapcommerce/SapcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.sapcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
